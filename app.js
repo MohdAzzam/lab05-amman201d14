742b8ca3ef6268165eaf99776db21179487b5c10
MohdAzzam <alazzamhmode@gmail.com>
2021-02-11 15:11:30 +0200

    fix issues

--- app.js
+++ app.js
@@ -143,14 +143,9 @@ let testDynamicArray = [1, 2, 3, 4, 5]; //eslint-disable-line
 
 function multiplyAnyArray(dynamicArray) {
   //eslint-disable-line
-  let mult=1;
-  for (let i = 0; i<testDynamicArray.length;i++){
-    mult*=testDynamicArray[i];
-  }
-
 }
 
 // Here is the test for multiplyArray(); uncomment it to run it
-// testMultiplyAnyArray(testDynamicArray);
+ testMultiplyAnyArray(testDynamicArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. You're done! Submit the link to the repo following the instructions in Canvas.
