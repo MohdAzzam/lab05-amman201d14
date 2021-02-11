3e5c711c7443bd05f3c972cf72e102e4eecf2679
MohdAzzam <alazzamhmode@gmail.com>
2021-02-11 13:17:10 +0200

    sum and multiplay done

--- app.js
+++ app.js
@@ -53,10 +53,15 @@ Test this function by hand in the console to get it working, and when you think 
 // Write your code here
 function sumAndMultiply(a, b, c) {
   //eslint-disable-line
+  let sumAandB=sum(a,b);
+  let allSum=sum(sumAandB[0],c);
+  let multAandB=multiply(a,b);
+  let allMult=multiply(multAandB[0],c);
+  return [allSum[0],allMult[0],`${a} and ${b} and ${c} sum to ${allSum[0]}.`,`The product of ${a} and ${b} and ${c} is ${allMult[0]}.`]
 }
 
 // Here is the test for sumAndMultiply(); uncomment it to run it
-// testSumAndMultiply(4,7,5);
+testSumAndMultiply(4,7,5);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
